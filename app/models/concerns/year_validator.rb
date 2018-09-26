89289ce9aba4436837add69f5b7a1499062febcf
yammik <mkim0514@gmail.com>
2018-09-26 10:55:31 -0400

    Done.

--- app/models/concerns/year_validator.rb
+++ app/models/concerns/year_validator.rb
@@ -1,7 +1,7 @@
 class YearValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     if value
-      unless value && value <= Time.new.year
+      unless value <= Time.new.year
         record.errors.add(attribute, "can't be from the future")
       end
     end
